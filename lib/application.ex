f6e110101bb5eab634e66a62d47dab738a406433
Mayel de Borniol <git@mayel.space>
2021-12-19 19:33:21 +1300

    small changes

--- lib/application.ex
+++ lib/application.ex
@@ -3,6 +3,7 @@ defmodule Bonfire.Application do
 
   @sup_name Bonfire.Supervisor
   @name Mix.Project.config()[:name]
+  @otp_app Bonfire.Common.Config.get!(:otp_app)
   @version Mix.Project.config()[:version]
   @repository Mix.Project.config()[:source_url]
   @deps Bonfire.Common.Extend.loaded_deps()
@@ -11,7 +12,7 @@ defmodule Bonfire.Application do
 
   def start(_type, _args) do
 
-    Bonfire.Repo.LogSlow.setup()
+    EctoSparkles.LogSlow.setup(@otp_app)
 
     :telemetry.attach("oban-errors", [:oban, :job, :exception], &Bonfire.ObanLogger.handle_event/4, [])
     Oban.Telemetry.attach_default_logger()
